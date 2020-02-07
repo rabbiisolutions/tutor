621b79a0c7af721e99129e0c7101bf8fdaf06c60
pcf26536 <muffwaindan@gmail.com>
2020-02-07 17:27:30 +0300

    update landing images [ch35]

--- src/events/BackDrop.js
+++ src/events/BackDrop.js
@@ -3,10 +3,8 @@ const hidden = 'hidden';
 const backDropHandler = e => {
   let modal = document.getElementsByClassName('sign-up modal')[0];
   const sideBar = document.getElementsByClassName('menu')[0];
-  //const services = document.getElementsByClassName('our-services')[0];
   modal.classList.add(hidden);
   sideBar.classList.add(hidden);
-  //services.classList.add(hidden);
   e.target.classList.toggle(hidden);
 };
 
