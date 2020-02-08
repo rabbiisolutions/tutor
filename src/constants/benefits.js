af590a80b05c7ef7aea252a5200c03ad98356748
pcf26536 <muffwaindan@gmail.com>
2020-02-08 09:05:47 +0300

    update benefits texts [ch35]

--- src/constants/benefits.js
+++ src/constants/benefits.js
@@ -5,27 +5,29 @@ import supportIcon from "../assets/icons/benefits/online-support.svg";
 
 const trusted = {
   title: 'Trusted Company', icon: trustedIcon,
-  desc: 'Lorem ipsum dolor sit amet, consectetur \n' +
-      'adipiscing elit. Scelerisque mi ac nisi, eu. Quam \n' +
-      'vitae etiam turpis eu cras. Vitae platea.'
+  desc: 'TeeCha is an agency that is trusted by teachers throughout the country for their great ' +
+      'service delivery and one that places the interests of the teacher above profit. We always' +
+      ' deliver our end of the bargain at the agreed time.'
 };
 const income = {
   title: 'Extra Source of Income', icon: incomeIcon,
-  desc: 'Lorem ipsum dolor sit amet, consectetur \n' +
-      'adipiscing elit. Scelerisque mi ac nisi, eu. Quam \n' +
-      'vitae etiam turpis eu cras. Vitae platea.'
+  desc: 'TeeCha gives an opportunity to teachers and trainers throughout the country to earn an' +
+      ' extra source of income by teaching what they are good at. One does not have to learn any' +
+      ' additional skill but simply maximize on what they already have.'
 };
 const relationship = {
   title: 'Great Relationship', icon: relationshipIcon,
-  desc: 'Lorem ipsum dolor sit amet, consectetur \n' +
-      'adipiscing elit. Scelerisque mi ac nisi, eu. Quam \n' +
-      'vitae etiam turpis eu cras. Vitae platea.'
+  desc: 'TeeCha prioritizes relationship above everything. We seek to maintain a good and mutual' +
+      ' benefiting relationship with our tutors. Because of this, we have a great relationship' +
+      ' with many teachers whom we consider partners in our quest to help people excel in all' +
+      ' they are learning.'
 };
 const support = {
   title: '24/7 Support', icon: supportIcon,
-  desc: 'Lorem ipsum dolor sit amet, consectetur \n' +
-      'adipiscing elit. Scelerisque mi ac nisi, eu. Quam \n' +
-      'vitae etiam turpis eu cras. Vitae platea.'
+  desc: 'You can always reach out to TeeCha at any time to consult or discuss any concerns. We have' +
+      ' our physical offices which you are always welcomed to pay us a visit. Apart from that you' +
+      ' can always get in touch with us through phone calls or WhatsApp. And depending on situation,' +
+      ' we can meet at a place of convenience.'
 };
 
 const benefits = {
